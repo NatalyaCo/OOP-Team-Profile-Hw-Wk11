01a43e6f393f261ca352e87a9fe9c0889a753bab
Natalya Colburn <greatqueenbee@gmail.com>
2022-05-09 12:46:27 -0600

    fixed test bug

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -1,31 +1,31 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
-    it("Can instantiate Employee instance", () => {
+    it("Should instantiate Employee instance", () => {
         const e = new Employee();
         expect(typeof(e)).toBe("object");
     });
 
-    it("Can set name via constructor arguments", () => {
+    it("Should set name via constructor arguments", () => {
         const name = "Natalya";
         const e = new Employee(name);
         expect(e.name).toBe(name);
     });
 
-    it("Can set id via constructor argument", () => {
+    it("Should set id via constructor argument", () => {
         const testValue = 100;
         const e = new Employee("Foo", testValue);
         expect(e.id).toBe(testValue);
     });
 
-    it("Can set email via constructor argument", () => {
+    it("Should set email via constructor argument", () => {
         const testValue = "test@test.com";
         const e = new Employee("Foo", 1, testValue);
         expect(e.email).toBe(testValue);
     });
 
     describe("getName", () => {
-        it("Can get name via getName()", () => {
+        it("Should get name via getName()", () => {
             const testValue = "Natalya";
             const e = new Employee(testValue);
             expect(e.getName()).toBe(testValue);
